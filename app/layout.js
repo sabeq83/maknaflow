871031a7e7742c9966e898a0fddedeb2cdd44d67
sabeq83 <sabeq83@gmail.com>
2026-08-12 16:04:40 +0700

    fix: suppress expected theme hydration difference

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
+    <html lang="id" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
